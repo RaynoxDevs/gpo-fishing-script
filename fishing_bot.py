d5825d296d26838b2fa0319baf1f15ebb51c6f05
raynox <iam.raynox@gmail.com>
2025-12-17 01:01:42 +0100

    was almost perfect w 105 as elif distance > 30:

--- fishing_bot.py
+++ fishing_bot.py
@@ -295,13 +295,13 @@ class GPOFishingBot:
         if distance > 150:
             return True, "long", 300   # Très très loin : monter vite
         elif distance > 100:
-            return True, "fast", 500   # Loin : monter contrôlé
+            return True, "fast", 450   # Loin : monter contrôlé
         elif distance > 80:
             return True, "fast", 255 # Loin : monter contrôlé
         elif distance > 50:
             return True, "hover", 135  # Moyennement loin : ralentir progressivement
         elif distance > 30:
-            return True, "hover", 105  # Moyennement loin : ralentir progressivement
+            return True, "hover", 95  # Moyennement loin : ralentir progressivement
         elif distance > 1:
             return True, "stable", 25 # Approche finale : stabilisation anticipée
         else:
